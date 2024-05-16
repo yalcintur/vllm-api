6b44147a271b17c6aa8f652511f1914371340e07
Yalçın Tür <42304303+yalcintur@users.noreply.github.com>
2024-05-16 01:45:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, workers=1)
+    uvicorn.run("vllm-api.app:app", host="0.0.0.0", port=8000, workers=1)
